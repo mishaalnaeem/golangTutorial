5302dad94a726af4e757cbdcfc429d761001008d
Mishaal Naeem <mishaalnaeem@gmail.com>
2023-12-06 11:01:59 +0200

    message sending and receiving added

--- chatserver.go
+++ chatserver.go
@@ -7,6 +7,10 @@ import (
 	"log"
 )
 
+//mapping for clients
+var client = make(map[string]net.Conn) //string to net.Conn mapping
+var messages = make(chan message)
+
 func main() {
 	//open port to listen to
 	listen, err := net.Listen("tcp", "localhost:9000")
@@ -33,10 +37,50 @@ func main() {
 	}
 }
 
-func broadcaster(){
+func broadcaster() {
 	//needs a channel for messages from connections 
+
+	for {
+		select {
+		case msg := <- messages:
+			for _, conn := range clients { //iterate over clients
+				//send message if client is not sneder
+				if msg.address == conn.RemoteAddr().String() {
+					continue
+				}
+				fmt.fPrintln(conn, msg.text)
+
+			}
+		}
+
+		//leaving messages
+	}
+
 }
 
-func handler(conn net,Conn){
-	//add connection record
+func handler(conn net.Conn) {
+	//add connection record, send messages <- handles each client
+
+	clients[conn.RemoteAddr().String()] = conn
+
+	//send a client joining message
+	messages <- newMessage("joined", conn)
+
+	input := bufio.NewScanner(conn)
+	for input.Scan () {
+		messages <- newMessage(": "+input.Text(), conn)
+	}
+
+	delete(clients, conn.Remote().String())
+
+	conn.Close()
+
+}
+
+func newMessage(msg string, conn net.Conn) message {
+	addr := conn.RemoteAddr.String()
+	return {
+		text: addr + msg,
+		address: addr,
+	}
 }
