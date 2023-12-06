f461a37853f0e58f8bad9addebceae161fa817ae
Mishaal Naeem <mishaalnaeem@gmail.com>
2023-12-06 23:11:09 +0200

    index.html, chat server|

---
+++ server.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/websocket"
+	"log"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+}
+
+var clients = make(map[string]websocket.Conn)
+
+func main() {
+	http.HandleFunc("/echo")
+	http.HandleFunc("/", serveStaticFile)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handlerConnection(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err!=nil {
+		log.Fatal(err)
+	}
+
+	clients[conn.RemoteAddr().String()] = conn
+
+	go handlerClient(conn)
+
+	go broadcaster()
+}
+
+func handlerClient(conn *websocket.Conn) {
+
+	//for closing connection when functions exits
+	defer func() {
+		delete(clients, conn.RemoteAddr().String())
+		conn.Close()
+	}
+
+	for {
+		msgType, msg, err := conn.ReadMessage()
+		if err!=nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%s: %s",conn.RemoteAddr().String(), string(msg))
+	}
+}
+
+func broadcaster() {
+	for {
+		for _, client := range clients {
+			if err = client.WriteMessage(msgType, msg); err!=nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
+
+func serverStaticFile(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
